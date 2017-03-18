d3fe37c83b9e45331943b5aa839509d679ccf697
Koichi Shiraishi <zchee.io@gmail.com>
2017-03-19 01:49:13 +0900

    cxx: add mutex badlock C++ source code token from the shinh/test
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ cxx/badlock6.cc
@@ -0,0 +1,47 @@
+// Code token from the https://github.com/shinh/test/blob/master/badlock6.cc
+// http://shinh.hatenablog.com/entry/20140601/1401561716
+
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+
+class C {
+public:
+  C() : done_(false), sum_(0) {
+    pthread_mutex_init(&mu_, NULL);
+    pthread_cond_init(&cond_, NULL);
+  }
+  void wait() {
+    pthread_mutex_lock(&mu_);
+    pthread_cond_wait(&cond_, &mu_);
+    pthread_mutex_unlock(&mu_);
+  }
+  void doSomething() {
+    pthread_mutex_lock(&mu_);
+    done_ = true;
+    pthread_mutex_unlock(&mu_);
+    pthread_cond_signal(&cond_);
+  }
+private:
+  pthread_mutex_t mu_;
+  pthread_cond_t cond_;
+  bool done_;
+  int sum_;
+};
+
+void* thread(void* data) {
+  C* c = (C*)data;
+  c->doSomething();
+  return NULL;
+}
+
+int main() {
+  C* c = new C();
+  pthread_t th;
+  pthread_create(&th, NULL, &thread, c);
+  // usleep(1);
+  c->wait();
+  pthread_join(th, NULL);
+  delete c;
+
+}
