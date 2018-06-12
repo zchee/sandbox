69335a18b62fe109c01d67d947c35bd3161aa4ac
Koichi Shiraishi <zchee.io@gmail.com>
2018-06-12 22:58:58 +0900

    c/rlimit: comment-out some lines

--- c/rlimit.c
+++ c/rlimit.c
@@ -7,12 +7,12 @@ int main (int argc, char *argv[])
 {
   struct rlimit limit;
 
-  limit.rlim_cur = 65535;
-  limit.rlim_max = 65535;
-  if (setrlimit(RLIMIT_NOFILE, &limit) != 0) {
-    printf("setrlimit() failed with errno=%d\n", errno);
-    return 1;
-  }
+  // limit.rlim_cur = 65535;
+  // limit.rlim_max = 65535;
+  // if (setrlimit(RLIMIT_NOFILE, &limit) != 0) {
+  //   printf("setrlimit() failed with errno=%d\n", errno);
+  //   return 1;
+  // }
 
   /* Get max number of files. */
   if (getrlimit(RLIMIT_NOFILE, &limit) != 0) {
